edf6e3dd043d59cc55211446a36a2f873b623386
João Vitor <joaoo.vittor007@gmail.com>
2020-11-09 00:23:15 -0300

    Estudo da liguagem JavaScript

--- JS-intermediario/aulaJS-4.js
+++ JS-intermediario/aulaJS-4.js
@@ -1,1 +1,15 @@
 // Aula 4
+
+// If e else
+
+
+const hora = 10;
+
+if (hora < 12) {
+  console.log("Bom dia!");
+} else if (hora > 12) {
+  console.log("Boa tarde");
+} else {
+  console.log("Boa noite!");
+}
+
--- JS-intermediario/aulaJS-5.js
+++ JS-intermediario/aulaJS-5.js
@@ -1,1 +1,16 @@
 // Aula 5
+
+// Operador Ternario
+
+const pontuacaoUsuario = 1000;
+
+// if (pontuacaoUsuario >= 1000) {
+//   console.log('Usuario VIP!');
+// } else {
+//   console.log('Usuario Comun!');
+// }
+
+//                                              true             false
+const usuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP!' : 'Usuario Comun!';
+
+console.log(usuario);
